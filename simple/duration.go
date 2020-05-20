451254055ac675ebe7f0772e3f0913ea015eb8c4
wangyongtao <wangyt4dev@gmail.com>
2020-05-20 14:55:53 +0800

    opt.

--- simple/duration.go
+++ simple/duration.go
@@ -6,7 +6,21 @@ import (
 )
 
 func main() {
+	fmt.Println(nowInMinute())
+	fmt.Println(defaultCollectInterval)
+	fmt.Println(defaultEvictInterval)
+	fmt.Println(myTestInterval)
+
 	connectTimeout := 5
 	time.Sleep(time.Duration(connectTimeout) * time.Second)
 	fmt.Printf("I have slept 5 sec.")
 }
+
+var nowInMinute = func() int64 {
+	return time.Now().Unix() / 60
+}
+var (
+	defaultCollectInterval = time.Second * 10
+	defaultEvictInterval   = time.Minute
+	myTestInterval         = time.Duration(10) * time.Second
+)
